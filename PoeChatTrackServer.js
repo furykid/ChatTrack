db0e2033c3169b80e7d308f4aa8eaf3ba38974e1
furykid <furykid777@gmail.com>
2018-02-25 04:23:51 -0500

    Fixed server for use with socket.io. Added handlers for getting params from web page (not working). Now able to create list on page :)

--- PoeChatTrackServer.js
+++ PoeChatTrackServer.js
@@ -1,66 +1,63 @@
 // Server to host log file changes
 // Author: Psychase
 
-const app = require('express')();  
-const server = require('http').Server(app);  
-const io = require('socket.io')(server);
-//const events = require('events');
-//const LogFileTracker = require('./TrackLogFile');
 
-//var NewLineEmitter = new events.EventEmitter();
+// Server Setup
+//---------------------------------------------
+//---------------------------------------------
+var express = require('express')
+  , http = require('http');
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+app.get('/', function(req, res) {  
+    res.sendFile(__dirname + '/index.html');
+});
+server.listen(8080);
+
+var mainSocket;
+io.on('connect', (socket) => {  
+    mainSocket = socket;
+});
+//---------------------------------------------
+//---------------------------------------------
+
+
+// Handle data
+//---------------------------------------------
+//---------------------------------------------
+io.on('sendData', (days, token) => {
+    // Test
+    const TestInputFilePath = 'C:\\Program Files (x86)\\Grinding Gear Games\\Path of Exile\\logs\\Client.txt';
+    LogFileTracker.TrackLogFile(TestInputFilePath, days, token);
+});
+
+const events = require('events');
+const LogFileTracker = require('./TrackLogFile');
+
+var NewLineEmitter = new events.EventEmitter();
+
 // List to make sure we don't rewrite messages
 // and reduce chatter.
-//var chatList = [];
-/*
+var chatList = [];
+
 LogFileTracker.on('new_log', (message) => {
     if(!chatList.includes(message)){
         chatList.push(message);
         NewLineEmitter.emit('list_update', message);
     }
 });
-*/
 
-app.get('/', (req, res) => {  
-    res.sendFile(__dirname + '/index.html');
+NewLineEmitter.on('list_update', (message) => {
+    if(mainSocket){
+        mainSocket.emit('event', message);
+    }
 });
+//---------------------------------------------
+//---------------------------------------------
 
-server.listen(8000);
 
-io.on('connection', (socket) => {  
-    socket.emit('announcements', { message: 'A new user has joined!' });
-});
-
-//http.createServer(function(req, res){
-    /*handleRequest(req);
-    fs.exists(indexFilePath, function (exist) {
-        if(!exist) {
-          // if the file is not found, return 404
-          res.statusCode = 404;
-          res.end(`File ${pathname} not found!`);
-          return;
-        }
-
-        fs.readFile(indexFilePath, (err, data) => {
-            if(err) {
-                res.statusCode = 500;
-                res.end('Error: ${err}')
-            }
-            else {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.end(data);
-            }
-        })
-    });
-    */
-    /*
-    NewLineEmitter.on('list_update', (message) => {
-        res.write(buildHTMLResponse());
-        //res.write('<li>' + message + '</li>' + '<br>');
-    });
-    */
-//    res.writeHead(200, {'Content-Type': 'text/html'});
-//    res.end(buildHTMLResponse());
-//}).listen(8000, '192.168.1.184'); 
 
 // Test
 //const TestInputFilePath = 'C:\\Program Files (x86)\\Grinding Gear Games\\Path of Exile\\logs\\Client.txt';
